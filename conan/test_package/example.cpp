2627e98d1e440cd55e783bb48d640dfbf30a3eaf
Ivan Gagis <igagis@gmail.com>
2023-05-17 09:41:15 +0300

    conan: fix test package

--- conan/test_package/example.cpp
+++ conan/test_package/example.cpp
@@ -3,7 +3,10 @@
 int main(int argc, const char** argv){
 	httpmodel::request req;
 
-	// TODO:
+	req.method = httpmodel::method::get;
+	req.url.path.push_back("dir1");
 
-	std::cout << "url = " << url.to_string() << std::endl;
+	req.headers.put("hdr1", "val1");
+
+	std::cout << "http headers = " << req.headers.get("hdr1").value() << std::endl;
 }
